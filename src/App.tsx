dce6422661559a65d7d04835956bf904d78ab1f1
Girish <girishpawar055@gmail.com>
2023-04-05 14:47:03 +0530

    Navbar added

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Typography, AppBar, Toolbar, Container } from '@mui/material';
+import { PhotoCamera } from '@mui/icons-material';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <AppBar position='relative'>
+          <Toolbar>
+            <PhotoCamera>
+              <Typography>
+                Photo Album
+              </Typography>
+            </PhotoCamera>
+          </Toolbar>
+        </AppBar>
+
+        <main>
+          <div>
+            <Container maxWidth = "sm">
+              <Typography variant='h2' align='center' color = "textPrimary" gutterBottom>
+                Photo Album
+              </Typography>
+              <Typography variant='h5' align = "center" color = "textSecondary" paragraph>
+                Hello everyone, Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa fugiat nemo, nam tempore accusantium magnam et delectus magni minus commodi voluptatibus velit est. Explicabo, eaque blanditiis! Quae ipsam iste dignissimos.
+              </Typography>
+            </Container>
+          </div>
+        </main>
     </div>
   );
 }
